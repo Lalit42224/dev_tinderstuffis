32ec99acb8cfcb3e334d0c710c8ab8f9cc03e839
lalit sharma <lalipandit8171@gmail.com>
2025-08-26 16:33:09 +0530

    Exploring get ,put,patch ,delete methods

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 const express = require("express")
 
 const app  = express();
-app.use("/test",(req,res)=>{
-         res.send("test h tera bro")
-});
+// app.use("/test",(req,res)=>{
+//          res.send("test h tera bro")
+// });
 
-app.use("/user",(req,res) => {
-         res.send("user h bhai tu")
+
+app.get("/user",(req,res)=>{
+         res.send("it is get Api")
+})
+app.post("/user",(req,res)=>{
+         res.send("data post ho gya")
+})
+app.put("/user",(req,res)=>{
+         res.send("data put kr to diya")
+})
+app.patch("/user",(req,res)=>{
+         res.send("patch kiya okay")
+})
+app.delete("/user",(req,res)=>{
+         res.send("tera naam delete h n")
 })
 
 
-app.listen(3000,()=>{
+
+app.listen(4000,()=>{
          console.log("server is sucssfully listening on port 3000")
 })
